87a6fa78e63998301b104ff706c4427511425613
amilkardev <melek.ferhi@gmail.com>
2019-04-14 04:24:31 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';  
+import { BackendService } from './backend.service';
 import { AppComponent } from './app.component';
-
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(BackendService)
   ],
   providers: [],
   bootstrap: [AppComponent]
